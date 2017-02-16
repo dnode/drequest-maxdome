2bac3db0b2c70da4f98e3d65227ad2477a35d8de
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-02-16 16:59:16 +0100

    Add SessionOptions to define how sessions works at maxdome

---
+++ src/SessionOptions.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class SessionOptions {
+  constructor(session) {
+    this.sessionId = session.sessionId;
+    this.customer = {
+      customerId: session.customer.customerId,
+    };
+  }
+
+  toRequestOptions(options) {
+    return {
+      headers: {
+        'mxd-session': this.sessionId,
+      },
+      url: {
+        path: options.url.path.replace('%customerId%', this.customer.customerId),
+      },
+    };
+  }
+}
+
+module.exports = SessionOptions;
